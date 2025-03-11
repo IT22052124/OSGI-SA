feac4590354b61617bcc9ed78a16a5bb5c46e203
Shadhir Fawz <129358224+ShadhirFawz@users.noreply.github.com>
2025-03-11 21:41:33 +0530

    Delete StudentCourseConsumer directory

--- StudentCourseConsumer/src/com/mtit/StudentCourseConsumer/StudentDashboard.java
+++
@@ -1,8 +0,0 @@
-package com.mtit.StudentCourseConsumer;
-
-import com.mtit.LecturePublisher.LecturePublisher;
-import com.mtit.ChapterPublisher.ChapterPublisher;
-
-public interface StudentDashboard {
-    void getServices(LecturePublisher lecturePublisher, ChapterPublisher chapterPublisher);
-}
