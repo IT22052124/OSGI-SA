0f540c63dcb78587bd4f012064628a860f8d608e
Shadhir Fawz <129358224+ShadhirFawz@users.noreply.github.com>
2025-03-11 21:21:47 +0530

    Delete LecturerConsumer directory

--- LecturerConsumer/src/com/mtit/LecturerConsumer/LecturerConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.mtit.LecturerConsumer;
-
-
-import com.mtit.LecturePublisher.Lecture;
-import com.mtit.ChapterPublisher.Chapter;
-
-public interface LecturerConsumer {
-    void reviewLectureAndChapter(Lecture lecture, Chapter chapter);
-}
