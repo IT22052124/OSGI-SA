fed123e6dc7316d13d6ba84c4f8239a081809e34
Shadhir Fawz <129358224+ShadhirFawz@users.noreply.github.com>
2025-03-11 21:22:03 +0530

    Delete ChapterPublisher directory

--- ChapterPublisher/src/com/mtit/ChapterPublisher/ChapterPublisher.java
+++
@@ -1,9 +0,0 @@
-package com.mtit.ChapterPublisher;
-
-
-import java.util.List;
-
-public interface ChapterPublisher {
-    List<Chapter> getChaptersByLectureId(int lectureId);
-    Chapter getChapterById(int chapterId);
-}
